69f1fc3020f724809ff7fc0cbfde5ba9493e9d66
João Lima <jmflima@gmail.com>
2022-05-27 17:07:49 -0300

    8_modulo_rotas

---
+++ 6_EXPRESS/8_modulo_rotas/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express() // inicializa o express
+const port = 3000
+
+const path = require('path')
+
+const userRouter = require('./users') //importando as rotas
+
+//ler o body
+app.use(
+    express.urlencoded({
+        extend: true,
+    })
+)
+
+app.use(express.json())
+
+const basePath = path.join(__dirname, 'templates')
+
+app.use('/users', userRouter)
+
+app.get('/', (req, resp) => {
+    resp.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
+
