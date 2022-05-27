9fe870e2351d9fdcdfded8ef06644df9dc663672
João Lima <jmflima@gmail.com>
2022-05-27 16:51:24 -0300

    4_criando_scripts

---
+++ 4_NPM_FUNDAMENTOS/4_criando_scripts/index.js
@@ -0,0 +1,12 @@
+const _ = require("lodash");
+const chalk = require("chalk");
+
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+
+const diff = _.merge(a, b);
+
+console.log(chalk.green.bold(diff));
+
+// para executar
+// npm start
