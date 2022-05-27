0b7c9e2ad4f32690ffe52449d0ddac0aeadaed2f
João Lima <jmflima@gmail.com>
2022-05-27 17:05:34 -0300

    5_middleware

---
+++ 6_EXPRESS/5_middleware/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express() // inicializa o express
+const port = 3000
+
+const path = require('path')
+
+const basePath = path.join(__dirname, 'templates')
+
+const checkAuth = function(req, res, next){
+    req.authStatus = true
+
+    if(req.authStatus) {
+        console.log("Está Logado")
+        next()
+    } else {
+        console.log("Não está logado. Fça login para continuar")
+        next()
+    }
+
+    app.use(checkAuth)
+}
+
+app.get('/', (req, resp) => {
+    resp.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
